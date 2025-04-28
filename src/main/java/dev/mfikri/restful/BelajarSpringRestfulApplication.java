9da22aaf8c11b216d16a0dfdba3e133f8cd9d8e3
abdullahfikri <mfikri.dev@gmail.com>
2025-04-28 15:38:02 +0700

    init: Initialize contact management REST API - Create the project - Add the Contact Management API Spesification

---
+++ src/main/java/dev/mfikri/restful/BelajarSpringRestfulApplication.java
@@ -0,0 +1,13 @@
+package dev.mfikri.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarSpringRestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mfikri/restful/BelajarSpringRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mfikri.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
